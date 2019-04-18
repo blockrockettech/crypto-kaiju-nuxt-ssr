b990a71a5027b8436be57d95e849d582320e59be
James Morgan <james.morgan.e@googlemail.com>
2019-04-18 10:21:20 +0100

    Fixing import

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -73,7 +73,7 @@ module.exports = {
       },
       {
         rel: "stylesheet",
-        href: "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.cs"
+        href: "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
       },
     ]
   },
